518eda2fb4393e7dc5db6650aa4ad164c2d62f84
Rys Rabang <rrabang@my.bcit.ca>
2019-09-27 14:33:30 -0700

    Initial Commit

---
+++ HateLines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HateLines
+//
+//  Created by Rys Rabang on 2019-09-27.
+//  Copyright © 2019 Rys Rabang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
