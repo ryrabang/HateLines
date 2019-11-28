03215faf4470717a2cb2ad37a5feda9ee5505818
Ivan <wmj900823@gmail.com>
2019-11-28 15:23:39 -0800

    add GlobalViewController and SearchViewcontroller

---
+++ HateLines/GlobalViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GlobalViewController.swift
+//  HateLines
+//
+//  Created by 王梦君 on 2019-11-28.
+//  Copyright © 2019 Rys Rabang. All rights reserved.
+//
+
+import UIKit
+
+class GlobalViewController : UIViewController {
+    
+    
+    @IBOutlet weak var commentsTableView: UITableView!
+    
+    var commentTableManager:PostsTableManager?
+    let posts:[Post] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        commentTableManager = PostsTableManager(connect: commentsTableView,withData: posts)
+    }
+    
+}
---
+++ HateLines/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  HateLines
+//
+//  Created by 王梦君 on 2019-11-28.
+//  Copyright © 2019 Rys Rabang. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
