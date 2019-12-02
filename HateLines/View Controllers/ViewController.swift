f2704f679f4db2d57e313913ce9078fdfbec3b79
momo <normalguyinvancouver@gmail.com>
2019-12-02 15:46:44 -0800

    added video and logo to landing page

--- HateLines/View Controllers/ViewController.swift
+++ HateLines/View Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
     
@@ -14,9 +15,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoLabel: UILabel!
+    
+    var videoPlayer: AVPlayer?
+    var videoPlayerLayer: AVPlayerLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // view?.backgroundColor = UIColor(white: 1, alpha: 0.5)
         
         setUpElements()
         
@@ -63,9 +70,49 @@ class ViewController: UIViewController {
     
     func setUpElements() {
         Utilities.styleFilledButton(signUpButton)
-        Utilities.styleHollowButton(loginButton)
+        Utilities.styleFilledButton(loginButton)
+        
+        logoLabel?.layer.cornerRadius = 20.0
+        logoLabel?.layer.masksToBounds = true
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setUpVideo()
+    }
     
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "hearts", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust size and frame
+        videoPlayerLayer?.frame = CGRect(x:
+            -self.view.frame.size.width*1.5, y: 0, width:
+            self.view.frame.size.width*4, height:
+            self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add to view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+    }
 }
 
